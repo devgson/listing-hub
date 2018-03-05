4ea7490dd6141246f5b0a3a41dd580d25b4d4662
Godson <godsonobielum@yahoo.com>
2018-03-05 23:10:55 +0100

    Commit

--- server/app.js
+++ server/app.js
@@ -3,12 +3,16 @@ const path = require('path');
 const routes = require('./routes/routes');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.set('view engine','pug');
 app.set('views', path.join(__dirname,'/views'))
 app.use( express.static(path.join(__dirname,'../public')) );
 
 app.use(routes);
+
+app.use((req, res) => {
+  res.render('404');
+});
 
 app.listen(port);
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,8 +2,40 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/404', async (req, res) => {
-  res.render('404');
+router.get('/category', async(req, res) => {
+  res.render('category');
+})
+
+router.get('/add-listing', async(req, res) => {
+  res.render('add-listing');
+});
+
+router.get('/edit-profile', async(req, res) => {
+  res.render('edit-profile');
+});
+
+router.get('/login', async(req, res) => {
+  res.render('login');
+});
+
+router.get('/manage-listing', async(req, res) => {
+  res.render('manage-listing');
+});
+
+router.get('/profile-detail', async(req, res) => {
+  res.render('profile-detail');
+});
+
+router.get('/search', async(req, res) => {
+  res.render('search');
+});
+
+router.get('/listing-detail', async(req, res) => {
+  res.render('listing-detail');
+});
+
+router.post('/login', async(req, res) => {
+  res.render('login');
 })
 
 module.exports = router;
